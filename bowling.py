afbcfba21a31084d9aefffb8e165bf6499d0c066
Laszlo Kiss <lklaszlokiss@gmail.com>
2017-08-06 15:12:23 +0200

    add is_spare function

--- bowling.py
+++ bowling.py
@@ -3,16 +3,16 @@ def score(game):
     frame = 1
     in_first_half = True
     for i in range(len(game)):
-        if game[i] == '/':
+        if is_spare(game[i]):
             result += 10 - last
         else:
             result += get_value(game[i])
         if frame < 10  and get_value(game[i]) == 10:
-            if game[i] == '/':
+            if is_spare(game[i]):
                 result += get_value(game[i+1])
             elif is_strike(game[i]):
                 result += get_value(game[i+1])
-                if game[i+2] == '/':
+                if is_spare(game[i+2]):
                     result += 10 - get_value(game[i+1])
                 else:
                     result += get_value(game[i+2])
@@ -35,7 +35,7 @@ def get_value(char):
         return int(char)
     elif is_strike(char):
         return 10
-    elif char == '/':
+    elif is_spare(char):
         return 10
     elif char == '-':
         return 0
@@ -44,3 +44,6 @@ def get_value(char):
 
 def is_strike(char):
     return char == 'X' or char == 'x'
+
+def is_spare(char):
+    return char == '/'
